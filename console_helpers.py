64f5bba545b526b1cf423deb14fc601255ede675
Mani <manivannan33@gmail.com>
2024-09-14 21:15:22 -0400

    Added another console utility module

---
+++ console_helpers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 11:22:56 2024
+Quick helpers. For example - close all open plot windows. These functions are
+meant to be called from the iPython console.
+@author: maniv
+"""
+import matplotlib.pyplot as plt
+
+def close():
+    plt.close('all')
