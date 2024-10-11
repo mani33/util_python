007ee897cb9310889c3282e48ca0de978cf6c4b9
Manivannan Subramaniyan <msubramaniyan@ds.bhsai.net>
2024-10-11 10:54:56 -0400

    console function added

---
+++ console_functions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 10:53:35 2024
+Module for commonly used functions in the console
+@author: msubramaniyan
+"""
+import matplotlib.pyplot as plt
+
+def cla():
+    plt.close('all')
+
